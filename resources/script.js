7b04cb58a51506841027219e47887f3d8b7652b6
jackbcjones <jackbcjones@gmail.com>
2022-07-07 11:56:54 +0200

    fixed dark mode and had it save to local storage

--- resources/script.js
+++ resources/script.js
@@ -2,12 +2,32 @@ const swup = new Swup();
 
 var dark = document.getElementById("dark");
 
-dark.onclick = function(){
 
+if(localStorage.getItem("theme") == null){
+    localStorage.setItem("theme", "light");
+}
+
+
+let localData = localStorage.getItem("theme");
+
+if(localData == "light") {
+    dark.innerHTML = "Dark";
+    document.body.classList.remove("dark-theme");
+}else if(localData == "night") {
+    dark.innerHTML = "Light";
+    document.body.classList.add("dark-theme");
+}
+
+dark.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
         dark.innerHTML = "Light"
+        localStorage.setItem("theme", "night");
+    } else {
+        dark.innerHTML = "Dark"
+        localStorage.setItem("theme", "light");
     }
+    
 }
 
 
