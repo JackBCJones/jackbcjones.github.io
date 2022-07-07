5c8ad42cbcef9a27ceb282bae4778c2c2674d1ca
jackbcjones <jackbcjones@gmail.com>
2022-07-07 11:09:03 +0200

    took away defer and added dark theme

--- resources/script.js
+++ resources/script.js
@@ -1,2 +1,13 @@
-import Swup from 'swup';
 const swup = new Swup();
+
+var dark = document.getElementById("dark");
+
+dark.onclick = function(){
+
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        dark.innerHTML = "Light"
+    }
+}
+
+
